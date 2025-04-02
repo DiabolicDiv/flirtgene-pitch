38d12c0ce5b78155d9454600b2558014430e8ac5
DiabolicDiv <vdcybercurity1007@gmail.com>
2025-04-03 02:24:05 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import '../styles/globals.css';
-
 export default function Home() {
   return (
     <div className="min-h-screen bg-red-500 text-white flex flex-col items-center justify-center px-4 py-12">
